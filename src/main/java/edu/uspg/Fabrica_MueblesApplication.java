9a5bd3a57eedb7330a43ae6afe3b43178cbd1f11
BryanSan117 <brsandovalc@alumno.uspg.edu.gt>
2021-08-21 12:43:10 -0500

    Se resolvio el error

--- src/main/java/edu/uspg/Fabrica_MueblesApplication.java
+++ src/main/java/edu/uspg/Fabrica_MueblesApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import Fabrica.Fabrica_Modernos;
 import Fabrica.Fabrica_Oficina;
+import Fabrica.Fabrica_Victorianos;
+import Interface.iMesa;
 import Interface.iSilla;
+import Interface.iSillon;
 
 
 
@@ -13,6 +16,10 @@ public class Fabrica_MueblesApplication {
 		
 	public static void main(String[] args) {
 		
+		System.out.println("************************************************");
+		System.out.println("************************************************");
+		System.out.println("************************************************");
+		
 		Fabrica_Modernos fabrica_M = new Fabrica_Modernos();
 		
 		iSilla silla = fabrica_M.getiSilla("Silla");
@@ -20,7 +27,31 @@ public class Fabrica_MueblesApplication {
 		silla.Asiento();
 		silla.Patas();
 		
+		System.out.println("************************************************");
+		System.out.println("************************************************");
+		System.out.println("************************************************");
 		
+		Fabrica_Oficina fabrica_O = new Fabrica_Oficina();
+		
+		iMesa mesa = fabrica_O.getiMesa("Mesa");
+		
+		mesa.Patas();
+		mesa.Tablero();
+		
+		System.out.println("************************************************");
+		System.out.println("************************************************");
+		System.out.println("************************************************");
+		
+		Fabrica_Victorianos fabrica_V = new Fabrica_Victorianos();
+		
+		iSillon sillon = fabrica_V.getiSillon("Sillon");
+		
+		sillon.Asiento();
+		sillon.Patas();
+		
+		System.out.println("************************************************");
+		System.out.println("************************************************");
+		System.out.println("************************************************");
 	}
 
 }
